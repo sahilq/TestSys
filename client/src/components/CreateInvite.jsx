215f9a3cfdb2196429f9c5eee57d531e9b2f9260
sahilq <iamsahilqureshi@gmail.com>
2019-02-28 13:39:29 +0530

    removed block is reduntant warning

--- client/src/components/CreateInvite.jsx
+++ client/src/components/CreateInvite.jsx
@@ -4,9 +4,14 @@ import { connect } from "react-redux";
 import * as actions from "../actions/actionCreators";
 
 class CreateInvite extends Component {
-  state = {};
+  state = {
+    date: Date,
+    isDate: false
+  };
 
   handleClick = e => {
+    if (!this.state.isDate) return alert("provide Date");
+    if (this.state.date < Date.now()) return alert("invalid date");
     const data = {
       testId: e.target.value,
       participantId: this.props.participantId,
@@ -15,35 +20,46 @@ class CreateInvite extends Component {
 
     this.props.createInv(data);
   };
-
+  onDateChange = e => {
+    console.log(e.target.value);
+    this.setState({ isDate: true });
+  };
   render() {
-    let arr = [];
-    {
-      this.props.invitedTo.map(invite => {
-        if (invite.participantId === this.props.participantId) {
-          arr.push(invite.testId);
-        }
-      });
+    //Array that contains testIds of Invites for a Particular Participant
+    let invitedTo = this.props.invitedTo;
+    let brr = [];
+    for (let invite in invitedTo) {
+      if (invitedTo[invite].participantId === this.props.participantId) {
+        brr.push(invitedTo[invite].testId);
+      }
     }
+    ////////////////
     return (
       <div className="container">
         <div className="row">
           <div className="col">
             <div>
               {this.props.test.testName}
-              {arr.includes(this.props.test._id) ? (
+              {brr.includes(this.props.test._id) ? (
                 <button className="btn btn-link float-right disabled">
                   Invited
                 </button>
               ) : (
-                <button
-                  className="btn btn-link float-right"
-                  onClick={this.handleClick}
-                  value={this.props.test._id}
-                  id={this.props.test.testName}
-                >
-                  Invite
-                </button>
+                <div>
+                  <input
+                    type="datetime-local"
+                    required={true}
+                    onChange={this.onDateChange}
+                  />
+                  <button
+                    className="btn btn-link float-right"
+                    onClick={this.handleClick}
+                    value={this.props.test._id}
+                    id={this.props.test.testName}
+                  >
+                    Invite
+                  </button>
+                </div>
               )}
             </div>
           </div>
