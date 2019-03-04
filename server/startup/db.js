875b4b2fad5c2e518f8fc7e5fe8445d5e611a94c
sahilq <iamsahilqureshi@gmail.com>
2019-03-04 15:32:33 +0530

    removed depricated warnings

--- server/startup/db.js
+++ server/startup/db.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 module.exports = function() {
   //using mongoose for database connection
   mongoose.connect("mongodb://localhost/TestSys", {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
   });
   const db = mongoose.connection;
   db.on("error", console.error.bind(console, "error connection"));
